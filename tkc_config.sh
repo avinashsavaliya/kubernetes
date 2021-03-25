8b80ec522f12fc2d2c5b7d656a2c29c6c7a35e16
Avinash <savaliya.avi@gmail.com>
2021-03-25 12:26:37 +0530

    Update tkc_config.sh

--- tkc_config.sh
+++ tkc_config.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+for i in $(seq ${1} ${2});
+do
+cluster=${3}-c$i
+echo $cluster
+template=`cat "tkc.yaml" | sed "s/{{MYNS}}/$targetns/g" | sed "s/{{MYNAME}}/$cluster/g" | sed "s/{{VERSION}}/${4}/g"`
+echo "$template" | kubectl apply -f - || echo "Command failed"
+done
