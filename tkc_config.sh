c2841843b2e81723275056d864eb9bf34e2c369c
Avinash <savaliya.avi@gmail.com>
2021-03-25 12:36:25 +0530

    Update tkc_config.sh

--- tkc_config.sh
+++ tkc_config.sh
@@ -7,6 +7,7 @@ cluster=${3}${i}-c$i
 echo $cluster
 template=`cat "tkc.yaml" | sed "s/{{MYNS}}/${3}${i}/g" | sed "s/{{MYNAME}}/$cluster/g" | sed "s/{{VERSION}}/${4}/g"`
 echo "$template" | kubectl apply -f - || echo "Command failed"
+sleep 300
 done
 
 # ./tkc_config.sh 2 2 antrea-19-7-ns 1.19.7+vmware.1-tkg.1.fc82c41
